e0073b389347b1631eaef4e493e7aac3e7709b26
Eliott Fertille <eliottfertille@MacBook-Pro-de-Eliott.local>
2024-11-11 16:18:25 +0100

    maj

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,21 @@
 <?php
 
 return [
+
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],  // URL de ton front-end Vite
+
+    'allowed_origins' => ['http://localhost:5173'],
+
     'allowed_origins_patterns' => [],
-    'allowed_headers' => ['Content-Type', 'X-Requested-With', 'Authorization', 'X-CSRF-TOKEN'],
+
+    'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
-    'supports_credentials' => true,  // Important si tu utilises des cookies ou l'authentification
+
+    'supports_credentials' => true,
+
 ];
